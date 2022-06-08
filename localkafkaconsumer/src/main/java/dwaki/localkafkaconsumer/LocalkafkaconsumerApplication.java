b3ba74b8b1d8b0ecea26597f7f997a410ec20208
Dwarakeshwaran Mageshkumar <30549378+Dwarakeshwaran@users.noreply.github.com>
2022-06-08 20:37:04 +0530

    Local Kafka Consumer Spring Boot App Added

---
+++ localkafkaconsumer/src/main/java/dwaki/localkafkaconsumer/LocalkafkaconsumerApplication.java
@@ -0,0 +1,13 @@
+package dwaki.localkafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalkafkaconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalkafkaconsumerApplication.class, args);
+	}
+
+}
---
+++ localkafkaconsumer/src/test/java/dwaki/localkafkaconsumer/LocalkafkaconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package dwaki.localkafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalkafkaconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
