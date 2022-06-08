85ba8cc64d957f152579fb0b7bafadbbeb111816
Dwarakeshwaran Mageshkumar <30549378+Dwarakeshwaran@users.noreply.github.com>
2022-06-08 15:14:58 +0530

    Local Kafka Producer Spring Boot Application Added

---
+++ localkafkaproducer/src/main/java/dwaki/localkafkaproducer/LocalkafkaproducerApplication.java
@@ -0,0 +1,13 @@
+package dwaki.localkafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalkafkaproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalkafkaproducerApplication.class, args);
+	}
+
+}
---
+++ localkafkaproducer/src/test/java/dwaki/localkafkaproducer/LocalkafkaproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package dwaki.localkafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalkafkaproducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
