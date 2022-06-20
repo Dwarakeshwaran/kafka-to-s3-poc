500c9ed9436fe9d4aa2eeaf98dc510a111bc1c70
Dwarakeshwaran Mageshkumar <30549378+Dwarakeshwaran@users.noreply.github.com>
2022-06-20 13:50:14 +0530

    Firehose Connector Project Added

---
+++ firehoseconnector/src/main/java/dwaki/firehoseconnector/FirehoseconnectorApplication.java
@@ -0,0 +1,13 @@
+package dwaki.firehoseconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirehoseconnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirehoseconnectorApplication.class, args);
+	}
+
+}
---
+++ firehoseconnector/src/test/java/dwaki/firehoseconnector/FirehoseconnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package dwaki.firehoseconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirehoseconnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
